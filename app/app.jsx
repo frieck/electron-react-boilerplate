c9d2692eaf69492200c2de4fd2bc5ada1403f00f
Felipe Augusto Rieck <felipe.rieck@gmail.com>
2016-08-02 08:32:32 -0300

    Initial code

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import { Button } from 'react-toolbox/lib/button';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('my-app')
+);
---
+++ app/helpers/edit_menu_template.ts
@@ -0,0 +1,12 @@
+export var editMenuTemplate :any = {
+    label: 'Edit',
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+};
